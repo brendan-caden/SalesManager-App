21847b269b7ce253c07bbb481eb6aed7302a5ce6
brendan <brendan@newplexus.co.uk>
2023-03-28 12:01:07 +0100

    New item added to database

--- src/main/resources/db/changelog/changelog_version-3.2.h2.sql
+++ src/main/resources/db/changelog/changelog_version-3.2.h2.sql
@@ -11,3 +11,7 @@ INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Leather sofa By Michellotti'
 --changeset JohnD:1582471835439-6
 INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Round coffee table By Michellotti', 1, 800.23);
 --rollback DELETE FROM SALES WHERE ITEM='Round coffee table By Michellotti';
+
+--changeset Brendan
+INSERT INTO SALES (ITEM, QUANTITY, AMOUNT) VALUES ('Pallet Chair by Picaso', 1, 2800.23);
+--rollback DELETE FROM SALES WHERE ITEM='Pallet Chair by Picaso';
